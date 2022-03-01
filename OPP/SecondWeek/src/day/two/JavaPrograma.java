a44ce24d2de37cd61e343ad3fc9e288aae30ba0e
Bandero3 <antekjanuszkiewicz@gmail.com>
2022-03-01 21:35:51 +0200

    New comment

--- OPP/SecondWeek/src/day/two/JavaPrograma.java
+++ OPP/SecondWeek/src/day/two/JavaPrograma.java
@@ -4,5 +4,6 @@ public class JavaPrograma {
     public static void main(String[] args) {
         System.out.println("Goodbye world");
         //Test
+        System.out.println("New comment");
     }
 }
